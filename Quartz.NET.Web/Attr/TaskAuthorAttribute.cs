33dad097d1371c00e968115551baa93bfb2e7449
jxx <283591387@qq.com>
2019-05-29 17:28:17 +0800

    1

---
+++ Quartz.NET.Web/Attr/TaskAuthorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quartz.NET.Web.Attr
+{
+    public class TaskAuthorAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
